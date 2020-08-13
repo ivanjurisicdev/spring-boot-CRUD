452d0fa39d3a5927e64d11bd4dfc0f0d2d916245
Ivan Jurisic <ivan@iconisagency.com>
2020-08-13 13:53:37 +0200

    Up

---
+++ src/main/java/net/codejava/AppController.java
@@ -0,0 +1,23 @@
+package net.codejava;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class AppController {
+    @Autowired
+    private ProductService service;
+
+    @RequestMapping("/")
+    public String viewHomePage(Model model) {
+        List<Product> listProducts = service.listAll();
+        model.addAttribute("listProducts", listProducts);
+
+        return "index";
+    }
+}
+
--- src/main/java/net/codejava/ProductService.java
+++ src/main/java/net/codejava/ProductService.java
@@ -1,11 +1,13 @@
 package net.codejava;
 
-import org.springframework.stereotype.Service;
-
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+    @Autowired
     private ProductRepository repo;
 
     public List<Product> listAll() {
