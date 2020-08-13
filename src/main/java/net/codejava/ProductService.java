235691bf82ee0b95d6d3fd6daee85d0f95b7c467
Ivan Jurisic <ivan@iconisagency.com>
2020-08-13 10:48:54 +0200

    Initial commit

---
+++ src/main/java/net/codejava/ProductService.java
@@ -0,0 +1,26 @@
+package net.codejava;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private ProductRepository repo;
+
+    public List<Product> listAll() {
+        return repo.findAll();
+    }
+
+    public void save(Product product) {
+        repo.save(product);
+    }
+
+    public Product get(Long id) {
+        return repo.findById(id).get();
+    }
+
+    public void delete(Long id) {
+        repo.deleteById(id);
+    }
+}
